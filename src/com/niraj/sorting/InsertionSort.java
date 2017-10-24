d6835ebf642c8d808c472abedcef91eadfad02e0
Niraj Talukdar <nirajtalukdar@gmail.com>
2017-10-25 00:54:01 +0100

    added more ways to apply insertion sorting

--- src/com/niraj/sorting/InsertionSort.java
+++ src/com/niraj/sorting/InsertionSort.java
@@ -2,6 +2,10 @@ package com.niraj.sorting;
 
 import java.util.Random;
 
+
+/**
+ * Insertion sort algorithm has a running time complexity of O(n^2) during worst case.
+ */
 public class InsertionSort {
 
     private int[] populateArray() {
@@ -15,6 +19,9 @@ public class InsertionSort {
         return array;
     }
 
+    /**
+     * Another way of applying the insertion sorting logic
+     */
     private void doInsertionSort(int[] array) {
 
         for (int i = 1; i < array.length; i++) {
@@ -34,6 +41,27 @@ public class InsertionSort {
         }
     }
 
+    /**
+     * Insertion sort algorithm is a stable algorithm if we keep the comparision of array[j] > current as written below.
+     * Stable algorithms ensure that equal values are not shifted from their order of occurrence during sorting. In this
+     * case, if we would have used '>=' instead of '>' for comparision, algorithm would behave as unstable as equal values
+     * would have been shifted.
+     */
+    private void doInsertionSorting(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            int current = array[i];
+            int j = i-1;
+
+            while (j>=0 && array[j] > current) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = current;
+        }
+    }
+
     private void displayArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]+" ");
@@ -46,7 +74,7 @@ public class InsertionSort {
         System.out.println("Array before sorting ==========> ");
         int[] input = is.populateArray();
         is.displayArray(input);
-        is.doInsertionSort(input);
+        is.doInsertionSorting(input);
         System.out.println("\nArray after sorting ==========>");
         is.displayArray(input);
     }
