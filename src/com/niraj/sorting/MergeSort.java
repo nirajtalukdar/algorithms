51002a224e3c7404b9c04057be2823cbb072b975
Niraj Talukdar <nirajtalukdar@gmail.com>
2017-10-30 12:15:17 +0000

    implemented temp array for holding sorted array

--- src/com/niraj/sorting/MergeSort.java
+++ src/com/niraj/sorting/MergeSort.java
@@ -15,6 +15,34 @@ public class MergeSort {
 
     private void doMerging(int[] array, int startIndex, int midIndex, int endIndex) {
 
+        int leftStartIndex = startIndex;
+        int rightStartIndex = midIndex+1;
+        int[] temp = new int[(endIndex-startIndex)+1];
+        int k = 0;
 
+        while(leftStartIndex <= midIndex && rightStartIndex <= endIndex) {
+
+            if (array[leftStartIndex] < array[rightStartIndex]) {
+
+                temp[k] = array[leftStartIndex];
+                leftStartIndex++;
+            } else {
+                temp[k] = array[rightStartIndex];
+                rightStartIndex++;
+            }
+            k++;
+        }
+
+        while (leftStartIndex <= midIndex) {
+            temp[k] = array[leftStartIndex];
+            leftStartIndex++;
+            k++;
+        }
+
+        while (rightStartIndex <= endIndex) {
+            temp[k] = array[rightStartIndex];
+            rightStartIndex++;
+            k++;
+        }
     }
 }
