ac9996be8626a78e49bbd5aa1c0f349b0bd04a4a
Niraj Talukdar <nirajtalukdar@gmail.com>
2017-12-09 17:41:08 +0000

    implemented the level order traversal

---
+++ src/com/niraj/trees/LevelOrderTraversal.java
@@ -0,0 +1,38 @@
+package com.niraj.trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+
+    class Node {
+
+        private int data;
+        private Node leftNode;
+        private Node rightNode;
+    }
+
+    public void doLevelOrderTraversing(Node node) {
+
+        if (node == null) {
+            return;
+        }
+        Queue<Node> nodeQueue = new LinkedList<>();
+        nodeQueue.offer(node);
+
+        while (!nodeQueue.isEmpty()) {
+
+            node = nodeQueue.peek();
+
+            if (node.leftNode != null) {
+                nodeQueue.offer(node.leftNode);
+            }
+            if (node.rightNode != null) {
+                nodeQueue.offer(node.rightNode);
+            }
+
+            System.out.println(node.data);
+            nodeQueue.poll();
+        }
+    }
+}
