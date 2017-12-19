2eb69c8773f17d4a63f51f6c70ebc47e44342d66
Niraj Talukdar <nirajtalukdar@gmail.com>
2017-12-19 23:37:11 +0000

    added comments and running time for VerifyBinarySearchTree

--- src/com/niraj/trees/VerifyBinarySearchTree.java
+++ src/com/niraj/trees/VerifyBinarySearchTree.java
@@ -1,6 +1,14 @@
 package com.niraj.trees;
 
 
+/**
+ * Algorithm to find whether a binary tree is a binary search tree (BST). A BST can have atmost
+ * 2 children and whose left child node has a value lesser than its root(parent) node and the
+ * right child node has a value larger than its root(parent) node. An empty node is also considered
+ * as a BST.
+ *
+ * The below algorithm has a running time complexity of 0(n)
+ */
 public class VerifyBinarySearchTree {
 
     private class Node {
@@ -10,6 +18,12 @@ public class VerifyBinarySearchTree {
         private Node rightNode;
     }
 
+    /**
+     * The algorithm compares each node data between its min and max range. A left node always needs
+     * to be between the min value and the data value of its root. Whereas, the right node always needs
+     * to be between the data value of its root node and the max value.
+     *
+     */
     public boolean isBinarySearchTree(Node node, int min_range, int max_range) {
 
         if (node == null) {
